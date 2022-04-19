9738ae56572a6a72d1b27e48e52c87405bf8a3e0
yudur <duarteyudi218@gmail.com>
2022-04-19 20:17:18 -0300

    for in em python

--- aula22/exemplo2.py
+++ aula22/exemplo2.py
@@ -1,10 +1,15 @@
-txt = ''
+txt = 'python"'
 nova_string = ''
 
+# continue — pula para o próximo laço
+# break — termina o laço
+
 for letra in txt:
-    if letra == '':
+    if letra == 'p':
         nova_string += letra.upper()
-    elif letra == 'h':
+    elif letra == 'y':
+        nova_string += letra.upper()
+    elif letra == 'n':
         nova_string += letra.upper()
     else:
         nova_string += letra
