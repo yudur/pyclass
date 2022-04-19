81bfbe4a03bd4c6d24333df5727b2b07d70ecc38
yudur <duarteyudi218@gmail.com>
2022-04-19 20:42:37 -0300

    Listas em python

---
+++ aula23/aula23.py
@@ -0,0 +1,14 @@
+"""
+Listas em python
+fatiamento
+append, insert, pop, del, clear, extend, +
+min, max
+range
+"""
+#         0    1    2    3    5
+lista = ['A', 'Bacana', 'C', 'D', 'E']
+#        -5   -4   -3   -2   -1
+string = 'a bacana cd'
+
+print(lista[1])
+print(string[2])
