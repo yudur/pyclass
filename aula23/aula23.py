2771858a026bedd875b1148660f3d1f5e9280c62
yudur <duarteyudi218@gmail.com>
2022-04-20 19:26:02 -0300

    Listas em python

--- aula23/aula23.py
+++ aula23/aula23.py
@@ -6,9 +6,14 @@ min, max
 range
 """
 #         0    1    2    3    5
-lista = ['A', 'Bacana', 'C', 'D', 'E']
-#        -5   -4   -3   -2   -1
+lista = ['A', 'B', 'C', 'D', 'E']
 string = 'a bacana cd'
 
+lista[1] = 'hoje esta um belo dia'
 print(lista[1])
 print(string[2])
+
+print()
+
+lista[1] = 'B'
+print(lista[:3])
