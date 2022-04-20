c23a8d5b7e18014dba39cf9c011d6bc7e5800b89
yudur <duarteyudi218@gmail.com>
2022-04-20 19:57:08 -0300

    Listas em python

--- aula23/exemplo2.py
+++ aula23/exemplo2.py
@@ -1,6 +1,17 @@
 lista_1 = [1, 2, 3]
 lista_2 = [4, 5, 6]
 lista_3 = lista_1 + lista_2
+
 print(lista_1)
 print(lista_2)
 print(lista_3)
+
+# extend — faz a junção de duas listas
+lista_1.extend(lista_2)
+print(lista_1)
+
+print()
+
+# append — adicionar um carácter no final da lista
+lista_1.append('yudi')
+print(lista_1)
