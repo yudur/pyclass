d7f89f0e905799fcab25e9c97336276e3295b681
yudur <duarteyudi218@gmail.com>
2022-04-21 18:10:51 -0300

    Listas em python

--- aula23/exemplo3.py
+++ aula23/exemplo3.py
@@ -5,13 +5,17 @@ while True:
     letra = input('digite uma letra: ')
 
     if len(letra) > 1:
-        print('ahh isso não vale, digite apenas uma letra')
+        print('ahh isso não vale, digite apenas uma letra.')
         continue
 
     elif not letra.isalpha():
-        print('ahh isso não vale, digite só uma letra')
+        print('ahh isso não vale, digite só uma letra.')
         continue
 
     digitadas.append(letra)
 
-    print(digitadas)
+    if letra in secreto:
+        print('Uau a letra "{}" existe na palavra secreta.  :) '.format(letra))
+
+    else:
+        print('aff a letra "{}" não existe na palavra secreta  :( ')
