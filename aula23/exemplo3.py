98ae69fae5c82c780c2e026cfbe428061e436614
yudur <duarteyudi218@gmail.com>
2022-04-21 17:55:33 -0300

    Listas em python

--- aula23/exemplo3.py
+++ aula23/exemplo3.py
@@ -8,12 +8,8 @@ while True:
         print('ahh isso não vale, digite apenas uma letra')
         continue
 
-    elif letra.isnumeric():
-        print('ahh isso não vale, {} não é uma letra'.format(letra))
-        continue
-
-    elif letra.isspace():
-        print('ahh isso não vale, digite apenas letras')
+    elif not letra.isalpha():
+        print('ahh isso não vale, digite só uma letra')
         continue
 
     digitadas.append(letra)
