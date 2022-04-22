00433696715cc7b240784d0c09d5f28e46c02691
yudur <duarteyudi218@gmail.com>
2022-04-22 18:11:12 -0300

    For / Else em python

--- aula24/exemplo2.py
+++ aula24/exemplo2.py
@@ -1,11 +1,8 @@
 pessoas = ['João', 'Alon', 'Lara', 'Yudi']
-existe_L = False
 
 for valor in pessoas:
-    if valor.startswith('L'):
-        existe_L = True
-
-if existe_L:
-    print('existe alguém com "L" no grupo!')
+    if valor.lower().startswith('l'):
+        print('existe alguém com "L" no grupo!')
+        break
 else:
     print('não existe ninguém com "L" no grupo')
