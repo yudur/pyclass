c245d35842453a080a53a6164f2f58a8eadec8c4
yudur <duarteyudi218@gmail.com>
2022-04-23 19:52:40 -0300

    Desempacotamento de listas em python

--- aula26/aula26.py
+++ aula26/aula26.py
@@ -2,7 +2,6 @@
 Desempacotamento de listas em python
 """
 lista = ['Marlon', 'Manu', 'Lene', 1, 2, 3, 4, 5, 6, 7, 8, 9, 100]
-*nova_list, n1, n2, n3 = lista
+n1, n2, n3, *_ = lista
 
-print(nova_list,)
-print(n1, n2, n3)
+print(f'{n1}, {n2}, {n3}.')
