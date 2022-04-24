0772a535e2b21987fb97c6502c50806dda23fe9a
yudur <duarteyudi218@gmail.com>
2022-04-24 13:34:50 -0300

    Expressões condicionais com o operador OR

---
+++ aula29/aula29.py
@@ -0,0 +1,20 @@
+"""
+Expressões condicionais com o operador OR
+"""
+nome = input('qual o seu nome? ')
+
+print(nome or 'você não digitou nada :(')
+
+print()
+
+a = 0
+b = None
+c = False
+d = []
+e = {}
+f = 22
+g = 'gustavo'
+
+variavel = a or b or c or d or e or f or g
+
+print(variavel)
