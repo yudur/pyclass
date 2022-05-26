0a63b07cf7d6b618c83c5658ad29b9cc6c00ba37
yudur <duarteyudi218@gmail.com>
2022-05-26 19:58:58 -0300

    Polimorfismo de sobreposição - Python Orientado a Objetos

---
+++ aula84/app.py
@@ -0,0 +1,28 @@
+"""
+Polimorfismo é o princípio que permite que classes derivadas de uma mesma
+superclasse tenham métodos iguais (de mesma assinatura), mas comportamentos
+diferentes.
+"""
+from abc import ABC, abstractmethod
+
+
+class A(ABC):
+    @abstractmethod
+    def fala(self, msg): pass
+
+
+class B(A):
+    def fala(self, msg):
+        print(f'B está falando: {msg}')
+
+
+class C(A):
+    def fala(self, msg):
+        print(f'C está falando: {msg}')
+
+
+b = B()
+c = C()
+
+b.fala('Bom dia')
+c.fala('Boa tarde')
