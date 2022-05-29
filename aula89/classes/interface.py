911baf2a979fdff4858c04c4c91354238cb06bf8
yudur <duarteyudi218@gmail.com>
2022-05-28 21:00:56 -0300

    Herança Simples (Tira-dúvidas) - Python Orientado a Objetos

---
+++ aula89/classes/interface.py
@@ -0,0 +1,6 @@
+from bibliotecas import Biblioteca
+
+
+class Interface(Biblioteca):
+    def metodo_da_interface(self):
+        print('Eu sou o método da Interface')
---
+++ aula89/main.py
@@ -0,0 +1,14 @@
+"""
+Herança Simples (Tira-dúvidas) - Python Orientado a Objetos
+
+Animal -> respirar
+    Lobo(animal) -> respirar / uivar
+    cachorro(Lobo) -> respirar / uivar / latir
+
+
+"""
+from classes.interface import Interface
+
+inter = Interface()
+inter.metodo_da_interface()
+inter.chama_metodo_da_interface()
