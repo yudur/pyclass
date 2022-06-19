a0e2b43d9788e1b70bde4409b6eb33bbef51b8d7
yudur <duarteyudi218@gmail.com>
2022-06-19 16:04:51 -0300

    Projeto Agenda: exibindos valores nas views

---
+++ aula128/contatos/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Categoria, Contato
+
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'categoria')
+    list_display_links = ('id', 'nome', 'sobrenome')
+    list_filter = ('categoria',)
+    list_per_page = 10
+    search_fields = ('nome', 'sobrenome', 'telefone')
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
