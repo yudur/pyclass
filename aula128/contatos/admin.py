92c2d4fec0730811977931de693767968be9b09d
yudur <duarteyudi218@gmail.com>
2022-06-20 15:59:22 -0300

    Ordenando e filtrando valores

--- aula128/contatos/admin.py
+++ aula128/contatos/admin.py
@@ -3,11 +3,13 @@ from .models import Categoria, Contato
 
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'categoria')
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email',
+                    'data_criacao', 'categoria', 'mostrar')
     list_display_links = ('id', 'nome', 'sobrenome')
     list_filter = ('categoria',)
     list_per_page = 10
     search_fields = ('nome', 'sobrenome', 'telefone')
+    list_editable = ('mostrar',)
 
 
 admin.site.register(Categoria)
--- aula128/contatos/views.py
+++ aula128/contatos/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Contato
+from django.core.paginator import Paginator
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True
+    )
+    paginator = Paginator(contatos, 10)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
     return render(request, 'contatos/index.html', {
         'contatos': contatos
+    })
+
+
+def detalhes(request, contato_id):
+    contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
+    return render(request, 'contatos/detalhes.html', {
+        'contato': contato
     })
