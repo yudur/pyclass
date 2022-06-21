77633082380619730aa0d8390950af3b390e8a3b
yudur <duarteyudi218@gmail.com>
2022-06-20 22:08:58 -0300

    Campo  de imagem

--- aula128/contatos/views.py
+++ aula128/contatos/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Contato
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 
 def index(request):
@@ -26,4 +28,28 @@ def detalhes(request, contato_id):
 
     return render(request, 'contatos/detalhes.html', {
         'contato': contato
+    })
+
+
+def busca(request):
+    termo = request.GET.get('termo')
+
+    if termo is None:
+        raise Http404()
+
+    campos = Concat('nome', Value(' '), 'sobrenome')
+
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+    )
+
+    paginator = Paginator(contatos, 10)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
     })
