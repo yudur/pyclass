653c8c5d00428b8d2cc7a349bb76a1471786f774
yudur <duarteyudi218@gmail.com>
2022-08-18 10:17:57 -0300

    validating numbers and emails

---
+++ aula141/main.py
@@ -0,0 +1,68 @@
+import re
+
+
+texto = '''
+Válidos
+0.0
+00.00
+000.000
++0.0
+-00.00
++000.000
+10
+50
+8.5
+-8.5
++10.5005412136
+1231345458.54654564
+-133215646589.6543215648978977
++1.11123123
+-0.154987
+1.121654987
+10.123
+10.1
+-1.1
+
+
+Inválidos
+10..2
+++1213
+--1235050
+.124546
+-.1231324
+10.
+.1
+.10
+10.
++10.
+-10.
+5a
+b5
+'''
+
+
+def is_float(string: str):
+    return bool(re.search(r'^[+-]?\d+(?:\.\d+)?$', string))
+
+def is_int(string: str):
+    return bool(re.search(r'^[+-]?\d+$', string))
+
+if __name__ == '__main__':
+    while True:
+        numero = input('digite um número:')
+
+        if is_int(numero):
+            try:
+                numero = int(numero)
+                print(f'O número {numero} foi convertido para int')
+            except:
+                print(f'O número {numero} não foi convertido para int')
+            continue
+
+        if is_float(numero):
+            try:
+                numero = float(numero)
+                print(f'O número {numero} foi convertido para float')
+            except:
+                print(f'O número {numero} não foi convertido para float')
+            continue
